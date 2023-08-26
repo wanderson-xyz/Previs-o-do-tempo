0d7bc3f88887b1fce6bc3faa2553121c7065526d
wanderson almeida <wandersonsa1110@gmail.com>
2023-08-25 23:39:12 -0300

    end feature

--- scripts.js
+++ scripts.js
@@ -4,12 +4,16 @@ const key = "7aae785226d12749805bc59f2711d7c8";
 
 function colocarDadosNaTela(dados) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temperatura").innerHTML = `${Math.floor(dados.main.temp)}° C`
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
     console.log(dados)
 }
 
 async function buscarCidade(cidade) {
 
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br`).then( resposta => resposta.json())
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
     colocarDadosNaTela(dados)
 
 }
